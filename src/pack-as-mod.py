ee1cfae73c589094a117417bd7c56f33ba6bade6
Mango <evan940415@gmail.com>
2024-08-05 15:07:00 +0800

    pref: update packing tool

--- src/pack-as-mod.py
+++ src/pack-as-mod.py
@@ -2,17 +2,15 @@ import zipfile
 from pathlib import Path
 
 CURRENT_DIR = Path(__file__).parent
-PROCESS_DIRS = {
-    "mod": "Simply-Create-Model-Mod-v1.2",
-}
+PROCESS_DIR = "mod"
+RESULT_FILENAME = "Simply-Create-Model-Mod-v1.2"
 
 
+current_folder_path = CURRENT_DIR / PROCESS_DIR
 
-for process_dir, result_filename in PROCESS_DIRS.items():
-    current_jar_path = CURRENT_DIR / f"{result_filename}.jar"
-    current_jar_path.unlink(missing_ok=True)
+current_jar_path = CURRENT_DIR / f"{RESULT_FILENAME}.jar"
+current_jar_path.unlink(missing_ok=True)
 
-    current_folder_path = CURRENT_DIR / process_dir
-    with zipfile.ZipFile(current_jar_path, "w") as zf:
-        for file in (current_folder_path).rglob("*"):
-            zf.write(file, file.relative_to(current_folder_path))
+with zipfile.ZipFile(current_jar_path, "w") as zf:
+    for file in (current_folder_path).rglob("*"):
+        zf.write(file, file.relative_to(current_folder_path))
