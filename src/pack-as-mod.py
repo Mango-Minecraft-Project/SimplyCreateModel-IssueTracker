425168ad602d8ba77ed5540667aa55a1212d64dc
Mango <evan940415@gmail.com>
2024-08-09 00:36:07 +0800

    feat: new generation location

--- src/pack-as-mod.py
+++ src/pack-as-mod.py
@@ -8,7 +8,7 @@ RESULT_FILENAME = "Simply-Create-Model-Mod-v1.2"
 
 current_folder_path = CURRENT_DIR / PROCESS_DIR
 
-current_jar_path = CURRENT_DIR / f"{RESULT_FILENAME}.jar"
+current_jar_path = CURRENT_DIR / "generated" / f"{RESULT_FILENAME}.jar"
 current_jar_path.unlink(missing_ok=True)
 
 with zipfile.ZipFile(current_jar_path, "w") as zf:
